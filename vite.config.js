980e1a36ccbe065008f1e84d843873a76a5b91aa
Egkdd <herrbreuer1987@gmail.com>
2025-03-26 14:09:17 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/Egkdd.github.io/",
   plugins: [react()],
-
-  esbuild: {
-    loader: "jsx",
-  },
-  resolve: {
-    alias: {
-      "./runtimeConfig": "runtimeConfig.browser",
-    },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        "js": "jsx",
-      },
-    },
-  },
-});
+})
