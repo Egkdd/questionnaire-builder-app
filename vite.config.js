be60810d73400fd568afe9692c88ef6ffb39c0f9
Egkdd <herrbreuer1987@gmail.com>
2025-03-26 13:48:25 +0200

    updated files

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,20 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/Egkdd.github.io/",
   plugins: [react()],
+
+  esbuild: {
+    loader: "jsx",
+  },
+  resolve: {
+    alias: {
+      "./runtimeConfig": "runtimeConfig.browser",
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        "js": "jsx",
+      },
+    },
+  },
 });
